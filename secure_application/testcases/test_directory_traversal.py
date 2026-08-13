9377e01fa3e65c209c9a0d566b6156a7daa0c93f
Adityaraj Shyamsundar Bhandari <sbadityaraj@gmail.com>
2026-08-13 12:25:04 +0530

    Implement directory traversal prevention test
    Add test for directory traversal prevention in secure_version.

--- secure_application/testcases/test_directory_traversal.py
+++ secure_application/testcases/test_directory_traversal.py
@@ -1,1 +1,15 @@
 
+import pytest
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+
+import secure_version
+
+def test_directory_traversal_prevention():
+    reports_dir = os.path.realpath(secure_version.REPORTS_DIRECTORY)
+    requested_path = os.path.realpath(os.path.join(reports_dir, "../README.md"))
+    
+    common_path = os.path.commonpath([reports_dir, requested_path])
+    assert common_path != reports_dir
