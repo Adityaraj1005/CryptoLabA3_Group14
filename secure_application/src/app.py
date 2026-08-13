eb5a654e921a12d3d6078fa00e1d6da746624a05
2024ucp1601-prog <2024ucp1601@mnit.ac.in>
2026-08-13 12:29:39 +0530

    Update app.py

--- secure_application/src/app.py
+++ secure_application/src/app.py
@@ -1,1 +1,14 @@
 
+from database import initialize_database
+from vulnerable_app import vulnerable_application
+
+
+def main():
+
+    initialize_database()
+
+    vulnerable_application()
+
+
+if __name__ == "__main__":
+    main()
