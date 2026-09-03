513481443985bd9803f7e2164a2e5c61b19bc891
Adityaraj Shyamsundar Bhandari <sbadityaraj@gmail.com>
2026-09-03 12:19:34 +0530

    Add functions for monoalphabetic cryptanalysis

--- attacks/monoalphabetic_attack/ src/cryptanalysis.h
+++ attacks/monoalphabetic_attack/ src/cryptanalysis.h
@@ -1,1 +1,14 @@
+#pragma once
+#include <bits/stdc++.h>
 
+using namespace std;
+
+string encrypt_monoalphabetic(const string& plaintext, const string& key);
+string generate_random_key();
+
+void frequency_analysis(const string& ciphertext);
+void word_frequency_analysis(const string& ciphertext);
+void pattern_analysis(const string& ciphertext);
+string apply_substitution(const string& ciphertext, const map<char, char>& key_map);
+void display_partial_plaintext(const string& partial_text);
+bool verify_solution(const string& original_ciphertext, const string& recovered_plaintext, const map<char, char>& key_map);
